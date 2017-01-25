023ce502b3138c6b2aae51d1e72dc7c25fc2b849
Arturo Jamaica <admin@example.com>
2017-01-24 19:46:05 -0600

    Finished API, initial parts for frontend

--- app.js
+++ app.js
@@ -4,19 +4,31 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var expressValidator = require('express-validator');
 
 var routes = require('./routes/index');
 
 var app = express();
 
+Date.prototype.valid = function() {
+  return isFinite(this);
+}
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
+app.use(expressValidator({
+ customValidators: {
+    isDate: function(value) {
+		d = new Date(value);
+        return d.valid();
+    }
+ }
+}));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ var express = require('express');
 var request = require('request');
 var async = require('async');
 var _ = require('underscore')
-
 var router = express.Router();
 
 
@@ -13,19 +12,29 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/*  
+	@Name : airlines
+	@Params : []
+*/
 router.get('/airlines', function(req, res, next) {
 
   request({ uri: host + '/airlines' }, function(err, response, body){
+	
 	if(err != undefined){
 			res.setHeader('Content-Type', 'application/json');
 			return res.status(500).send({ "error" : 500 , "description" : "Server Error"});
 	}
+	
 	res.setHeader('Content-Type', 'application/json');
   	res.send(body);
   })
   
 });
 
+/*  
+	@Name : airports
+	@Params : []
+*/
 router.get('/airports', function(req, res, next) {
   if(err != undefined){
 		res.setHeader('Content-Type', 'application/json');
@@ -38,14 +47,39 @@ router.get('/airports', function(req, res, next) {
 
 });
 
+/*
+	@Name : search
+	@Params : [date ,from ,to ]
+	@description : 
+	@comments : This method may be big but I really want to make it work as a backend api is deploy. Thats why I am validating everything.  
+	
+*/
 router.get('/search', function(req, res, next) {
  	
+	// Validator params
+	req.checkQuery('date', 'Date must be provided').notEmpty();
+	req.checkQuery('from', 'Origin must be provided').notEmpty();
+	req.checkQuery('to', 'Destination must be provided').notEmpty();
+	// Custom Date Validator
+	req.checkQuery('date', 'Must be a valid date').isDate()
+	
+	var date = req.query['date'];
+	var from = req.query['from'];
+	var to = req.query['to'];
+	
+	var errors = req.validationErrors();
+	if (errors) {
+	  	var errors_array = errors;
+		errors_array["error"] = 400
+		return res.status(400).send(errors_array);
+	}
+	
 	// Get Airlines
 	request({ uri: host + '/airlines' }, function(err, response, body){
-		
+
 		if(err != undefined){
 			res.setHeader('Content-Type', 'application/json');
-			return res.status(500).send({ "error" : 500 , "description" : "Server Error"});
+			 res.status(500).send({ "error" : 500 , "description" : "Server Error"});
 		}
 		
 		var parsed_airlines = JSON.parse(body);
@@ -54,14 +88,14 @@ router.get('/search', function(req, res, next) {
 		
 		// Prepare all uls to target
 		for (var i=0; i < parsed_airlines.length; i++) {
-			urls.push(host + '/flight_search/' + parsed_airlines[i]["code"] + '?date=2018-09-02&from=SYD&to=JFK')
+			urls.push(host + '/flight_search/' + parsed_airlines[i]["code"] + '?date='+ date +'&from='+from+'&to=' + to)
 		};
-		
+
 		// Prepare callbacks for parallel
 		for(var i = 0; i < urls.length; i++){
-		    (function(URLIndex){
+		    (function(index){
 		       queue.push(function(callback){
-		           request(urls[URLIndex], function(error, response, json) {
+		           request(urls[index], function(error, response, json) {
 		               callback(error, json);
 		           });
 		       });
@@ -85,14 +119,12 @@ router.get('/search', function(req, res, next) {
 			 });
 			
 			to_render = _.flatten(to_render, true);
-
 			res.setHeader('Content-Type', 'application/json');
 			return res.send(to_render);
 
 		});
 		
 	});
-		
 	
 });
 
